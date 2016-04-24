ced846e9fbe10d3c773109948693f916a09032e5
Bruno S Reis <bruno4box5@gmail.com>
2016-04-24 15:59:29 -0300

    added service to fetch github profile and changed view to display it

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,23 @@
-import {Page} from 'ionic-angular';
+import {Page} from 'ionic-angular'
+import {GitHubService} from '../../services/github'
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [GitHubService]
 })
-export class HomePage {
-  constructor() {
 
+export class HomePage {
+  public foundRepos
+  public username
+
+  constructor(private github: GitHubService) {
+  }
+
+  getRepos() {
+    this.github.getRepos(this.username).subscribe(
+      data => { this.foundRepos = data.json()},
+      err => console.error(err),
+      () => console.log('getRepos completed')
+    )
   }
 }
